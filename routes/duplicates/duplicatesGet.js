cfc9b15e8254068193f50e8524785aadff3f27fd
klequis <klequis@gmail.com>
2020-10-25 08:06:17 -0700

    improve formatting

--- routes/duplicates/duplicatesGet.js
+++ routes/duplicates/duplicatesGet.js
@@ -7,7 +7,10 @@ import { redf, yellow } from 'logger'
 
 const duplicatesGet = wrap(async (req, res) => {
   try {
-    const data = await find(TRANSACTIONS_COLLECTION_NAME, { duplicate: true })
+    const data = await find(
+      TRANSACTIONS_COLLECTION_NAME, 
+      { duplicate: true }
+    )
     res.send({ data: convertFieldValuesToUi(data), error: null })
   } catch (e) {
     throw e
