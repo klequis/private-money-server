f76a7c56961b9aebd50d4e41009252e5d6322455
klequis <klequis@gmail.com>
2020-10-26 09:04:52 -0700

    Additional debug functions

--- routes/views/allTransactionsByDescription.js
+++ routes/views/allTransactionsByDescription.js
@@ -7,21 +7,43 @@ import {
 // @ts-ignore
 import { yellow, red, redf } from 'logger'
 
+const moreThan2Rules = (data) => data.filter(t => {
+  if (t.ruleIds === undefined) {
+    // console.log(t.ruleIds)
+    return true
+  }
+  if (t.ruleIds.length > 1) {
+    return true
+  }
+  return false
+})
+
+const noRules = (data) => data.filter(t => {
+  if (t.ruleIds === undefined) {
+    return true
+  } else if (t.ruleIds.length === 0) {
+    return true
+  }
+  return false
+})
+
+const hasRules = (data) => data.filter(t => {
+  if (t.ruleIds === undefined) {
+    return false
+  }
+  if (t.ruleIds.length === 0) {
+    return false
+  }
+  return true
+})
+
+
 const allDataByDescription = wrap(async (req, res) => {
   // try {
-    const data = await find(TRANSACTIONS_COLLECTION_NAME, {})
-    // TODO: tmp code
-    const moreThan2 = data.filter(t => {
-      if (t.ruleIds === undefined) {
-        // console.log(t.ruleIds)
-        return true
-      }
-      if (t.ruleIds.length > 1) {
-        return true
-      }
-      return false
-    })
-    res.send({ data: convertFieldValuesToUi(moreThan2), error: null })
+  const data = await find(TRANSACTIONS_COLLECTION_NAME, {})
+  // TODO: tmp code
+  // const filtered = hasRules(data)
+  res.send({ data: convertFieldValuesToUi(data), error: null })
   // } catch (e) {
   //   // redf('views/allDataByDescription', e.message)
   //   throw e
