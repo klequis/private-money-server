aa046aac67ee250b0b6f32c2e1ec5c17ff966699
klequis <klequis@gmail.com>
2021-07-06 11:11:41 -0700

    add env: 'prod-local'

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,10 @@ module.exports = {
       script: './server/index.js',
       instances: 1,
       env: {
-        NODE_ENV: 'development' // local db
+        // NODE_ENV: 'development' // local db
         // NODE_ENV: 'production' // atlas
         // NODE_ENV: 'demo' // local
+        NODE_ENV: 'prod-local'
       }
     }
   ]
